f21adda286723e63356c4fba3956e6fa0cd92252
bela <1981253197@qq.com>
2020-10-08 20:26:40 +0800

    update

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',
-    app_id: "wxf68f9cb6777cee8d",
-    app_secret: "59f88a265b58787e40a2574dbdc96471",
-    t_app_id: "W6MJEGo4dZwuwcxPWhyyXMuJ",
-    t_app_secret: "cTwnNJu2auHJza6YZGc94TiZ"
-  }
-})
